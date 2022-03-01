b1cb5312d6230c7ca31acd040d269e1e25588010
Davi Silva <davi.silva2597@gmail.com>
2022-03-01 14:05:32 -0300

    Add Model PlanoVip

---
+++ XInvestimento/Models/PlanoVip.cs
@@ -0,0 +1,10 @@
+﻿namespace XInvestimento.Models
+{
+    public class PlanoVip
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public double Preco { get; set; }
+        public virtual ICollection<ClientesPlano> ClientesPlano { get; set; }
+    }
+}
