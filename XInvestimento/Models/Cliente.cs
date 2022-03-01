3fc67be55fe0069804068f2cbdcd04cf8592c726
Davi Silva <davi.silva2597@gmail.com>
2022-03-01 14:04:22 -0300

    Add Model Cliente

---
+++ XInvestimento/Models/Cliente.cs
@@ -0,0 +1,14 @@
+﻿namespace XInvestimento.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string NomeCompleto { get; set; }
+        public DateTime DataNacimento { get; set; }
+        public string Cpf { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public virtual ICollection<ClientesPlano> ClientesPlano { get; set; }
+        public virtual ICollection<Endereco> Endereco { get; set; }
+        public virtual ICollection<DadosFinaceiro> DadosFinaceiro { get; set; }
+    }
+}
