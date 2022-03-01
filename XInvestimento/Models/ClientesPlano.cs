e71261eacb18f280a26cdb3a1d3d3e7c3043ce6b
Davi Silva <davi.silva2597@gmail.com>
2022-03-01 14:04:44 -0300

    Add Model ClientesPlano

---
+++ XInvestimento/Models/ClientesPlano.cs
@@ -0,0 +1,11 @@
+﻿namespace XInvestimento.Models
+{
+    public class ClientesPlano
+    {
+        public int Id { get; set; }
+        public int IdCliente { get; set; }
+        public int IdPlanoVip { get; set; }
+        public virtual Cliente Cliente { get; set; }
+        public virtual PlanoVip PlanoVip { get; set; }
+    }
+}
