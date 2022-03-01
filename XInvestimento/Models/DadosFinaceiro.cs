6ee4d61c678d61982abc39ac9d34ca9fb8b7cae5
Davi Silva <davi.silva2597@gmail.com>
2022-03-01 14:05:03 -0300

    Add Model DadosFinaceiro

---
+++ XInvestimento/Models/DadosFinaceiro.cs
@@ -0,0 +1,12 @@
+﻿namespace XInvestimento.Models
+{
+    public class DadosFinaceiro
+    {
+        public int Id { get; set; }
+        public string Conta { get; set; }
+        public string Agencia { get; set; }
+        public double RendaMensal { get; set; }
+        public int IdCliente { get; set; }
+        public virtual Cliente Cliente { get; set; }
+    }
+}
