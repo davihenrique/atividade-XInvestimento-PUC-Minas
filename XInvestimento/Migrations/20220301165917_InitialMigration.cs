db91cede3a7692542a850ea6de4f9dbc146a5f62
Davi Silva <davi.silva2597@gmail.com>
2022-03-01 14:07:43 -0300

    Add InitialMigration

---
+++ XInvestimento/Migrations/20220301165917_InitialMigration.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace XInvestimento.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeCompleto = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    DataNacimento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CPF = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    DataCadastro = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PlanoVip",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descricao = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Preco = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlanoVip", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EnderecoCliente",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Logradouro = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    Bairro = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    Cidade = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    UF = table.Column<string>(type: "varchar(2)", unicode: false, maxLength: 2, nullable: false),
+                    CEP = table.Column<string>(type: "varchar(9)", unicode: false, maxLength: 9, nullable: false),
+                    Complemento = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    IdCliente = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EnderecoCliente", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Endereco",
+                        column: x => x.IdCliente,
+                        principalTable: "Clientes",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FinancieroCliente",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Conta = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Agencia = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RendaMensal = table.Column<double>(type: "float", nullable: false),
+                    IdCliente = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FinancieroCliente", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FinanceiroCliente_Cliente",
+                        column: x => x.IdCliente,
+                        principalTable: "Clientes",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ClientesPlano",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdCliente = table.Column<int>(type: "int", nullable: false),
+                    IdPlanoVip = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClientesPlano", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cliente",
+                        column: x => x.IdCliente,
+                        principalTable: "Clientes",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_PlanoVip",
+                        column: x => x.IdPlanoVip,
+                        principalTable: "PlanoVip",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClientesPlano_IdCliente",
+                table: "ClientesPlano",
+                column: "IdCliente");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClientesPlano_IdPlanoVip",
+                table: "ClientesPlano",
+                column: "IdPlanoVip");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EnderecoCliente_IdCliente",
+                table: "EnderecoCliente",
+                column: "IdCliente");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FinancieroCliente_IdCliente",
+                table: "FinancieroCliente",
+                column: "IdCliente");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ClientesPlano");
+
+            migrationBuilder.DropTable(
+                name: "EnderecoCliente");
+
+            migrationBuilder.DropTable(
+                name: "FinancieroCliente");
+
+            migrationBuilder.DropTable(
+                name: "PlanoVip");
+
+            migrationBuilder.DropTable(
+                name: "Clientes");
+        }
+    }
+}
