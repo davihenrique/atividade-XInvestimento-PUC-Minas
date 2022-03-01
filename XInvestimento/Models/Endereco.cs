d054f91366ab6b7fcdf6a9c89264c9faed27774b
Davi Silva <davi.silva2597@gmail.com>
2022-03-01 14:05:18 -0300

    Add Model Endereco

---
+++ XInvestimento/Models/Endereco.cs
@@ -0,0 +1,15 @@
+﻿namespace XInvestimento.Models
+{
+    public class Endereco
+    {
+        public int Id { get; set; }
+        public string Logradouro { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string UF { get; set; }
+        public string CEP { get; set; }
+        public string Complemento { get; set; }
+        public int IdCliente { get; set; }
+        public virtual Cliente Cliente { get; set; }
+    }
+}
